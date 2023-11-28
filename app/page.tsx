4fc6d6ba46a42fa4bd35c03a9b9172069d63a527
Frank Tan <tansongyang@gmail.com>
2023-11-28 11:55:04 -0500

    Add hero image

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Image from 'next/image'
+
 export default function Home() {
-  return <p className="text-center">Body</p>
+  return (
+    <div>
+      <Image src="/hero.gif" width={400} height={300} alt="Funny GIF" />
+      <p className="text-center">Body</p>
+    </div>
+  )
 }
