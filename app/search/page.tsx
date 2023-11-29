87b9e11c8ba2f1bacea1c05e406536d2f52d21a3
Frank Tan <tansongyang@gmail.com>
2023-11-29 14:48:39 -0500

    Add rudimentary search form on home page

---
+++ app/search/page.tsx
@@ -0,0 +1,9 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Pokécommerce | Find a storefront',
+}
+
+export default function Home() {
+  return <p className="text-center">Search</p>
+}
