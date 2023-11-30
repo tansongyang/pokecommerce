cf4830211fb48febaeef571e7dec00e335d4971b
Frank Tan <tansongyang@gmail.com>
2023-11-30 15:56:46 -0500

    Populate search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,28 @@
 import { Metadata } from 'next'
 
+import { fetchLocations } from '@/app/lib/data'
+
 export const metadata: Metadata = {
   title: 'Pokécommerce | Find a storefront',
 }
 
-export default function Home() {
-  return <p className="text-center">Search</p>
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { zip: string }
+}) {
+  const locations = await fetchLocations(searchParams.zip)
+  return (
+    <ul className="flex flex-col gap-y-4 p-4">
+      {locations.map((l) => (
+        <li
+          key={l.id}
+          className="flex justify-between rounded-3xl border-2 border-primary p-4"
+        >
+          <p>{l.name}</p>
+          <p>{`${l.distance} mi`} </p>
+        </li>
+      ))}
+    </ul>
+  )
 }
